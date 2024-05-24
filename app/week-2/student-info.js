341e3f1b0ca5886b4eb2e5baf250f857120a7662
CankunNa <cankun.na@gmail.com>
2024-05-23 20:59:51 -0600

    Assignment1_first_commit
    Set the home page and add a week-2 page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+
+
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+      <div>
+        <h1>Cankun Na</h1>
+        <ul>
+        <li><Link href= "https://github.com/CankunNa/cprg306-assignments"> https://github.com/CankunNa/cprg306-assignments</Link></li>
+      </ul>
+
+      </div>
+    );
+  }
